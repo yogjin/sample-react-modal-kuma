978b8984807fdcb952475e369e1c9732c861d374
yogjin <youngjin988@gmail.com>
2023-05-02 14:24:14 +0900

    styled components 적용

--- src/lib/KumaModal.tsx
+++ src/lib/KumaModal.tsx
@@ -1,5 +1,5 @@
 import React, { PropsWithChildren, ReactElement, useState } from 'react';
-import './styles.css';
+import styled from 'styled-components';
 
 type Props = {
   trigger: ReactElement;
@@ -20,12 +20,41 @@ export const KumaModal = ({ trigger, children }: PropsWithChildren<Props>) => {
     <>
       {React.cloneElement(trigger, { onClick: handleOpenModal })}
       {isModalOpen && (
-        <div className="modal-overlay" onClick={handleCloseModal}>
-          <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <ModalOverlay onClick={handleCloseModal}>
+          <Modal className="modal" onClick={(e) => e.stopPropagation()}>
             {children}
-          </div>
-        </div>
+          </Modal>
+        </ModalOverlay>
       )}
     </>
   );
 };
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Modal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+  width: 500px; /* 수정 */
+  height: 500px; /* 수정 */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
